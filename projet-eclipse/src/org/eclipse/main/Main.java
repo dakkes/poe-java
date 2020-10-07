6884ab9f8fe64a1b1cde79549adc05a571655e1d
dakkes <dakkesabdalohabe@gmail.com>
2020-10-07 09:59:54 +0200

    initialisation d'un projet java sous eclipse

---
+++ projet-eclipse/src/org/eclipse/main/Main.java
@@ -0,0 +1,11 @@
+package org.eclipse.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world from Eclipse");
+
+	}
+
+}
